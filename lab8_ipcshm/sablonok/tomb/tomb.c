dabbcc51ef0c35468539cdf447537502f979cee2
domokos.balazs <domokos.balazs@student.ms.sapientia.ro>
2026-05-25 15:19:30 +0300

    Exercising done, solved a ton from lab8

--- lab8_ipcshm/sablonok/tomb/tomb.c
+++ lab8_ipcshm/sablonok/tomb/tomb.c
@@ -14,12 +14,29 @@
 #define N 10 //tömb hossza
 #define MS 20 //időzítés kritikus szakasz 
 
+typedef struct
+{
+    int arr[N];
+    int index;
+}MEMORY;
+
+MEMORY *mem;
+
+int semid;
+
+
 void fiu_feladata(int i)
 {
+    struct sembuf fiu_down[] = {{0, -1, 0}, {1, -1, 0}};
+    struct sembuf fiu_up = {0, +1, 0};
     printf("start: %d fiú pid=%d, csoport=%d\n", i, getpid(), getpgrp());
     while(1){
         //TODO: a fiúk feladata
+
+        semop(semid, fiu_down, 2);
+        mem->arr[mem->index++] = i;
         usleep(10000);
+        semop(semid, &fiu_up, 1);
     }
     printf("end: fiú pid=%d\n", getpid());
 }
@@ -37,6 +54,22 @@ int main(int argc, char **argv)
     printf("apa pid=%d, csoport=%d\n", getpid(), getpgrp());
 
     //TODO: előkészítés: szemafor + szemafor init + memória + felcsatolás
+
+    int shmid=shmget(IPC_PRIVATE, sizeof(MEMORY), IPC_CREAT | 0666);
+    if(shmid < 0)
+        syserr("shmget");
+
+    mem = (MEMORY *)shmat(shmid, NULL, 0);
+    if((void *)mem == (void *)-1)
+        syserr("shmat");
+
+    semid = semget(IPC_PRIVATE, 2, IPC_CREAT | 0666);
+    if(semid < 0)
+        syserr("semget");
+
+    int init[] = {1, N};
+    if(semctl(semid, 0, SETALL, init) < 0)
+        syserr("semctl");
 
     int i;
     for (i = 0; i < K; i++)
@@ -60,6 +93,22 @@ int main(int argc, char **argv)
 
     //TODO: apa feladata
     //apa kód
+    if(argc != 2)
+        printf("Kell paraméterként egy szám, hogy hányszor fusson!");
+
+    struct sembuf apa_wait_for_full[] = {{0, -1, 0}, {1, 0, 0}};
+    struct sembuf apa_up[] = {{0, +1, 0}, {1, +N, 0}};
+
+    for(i = 0; i < atoi(argv[1]); i++){
+        semop(semid, apa_wait_for_full, 2);
+        for(int j=0; j < N; j++){
+            printf("%d ", mem->arr[j]);
+        }
+        printf("\n");
+        mem->index = 0;
+        semop(semid, apa_up, 2);
+    }
+
 
     usleep(300000); //a fiúk induljanak el, ha nem vár, lehet, hogy még nem futnak 
 
@@ -78,6 +127,14 @@ int main(int argc, char **argv)
     }
 
     free(pids);
+    if(shmdt((void *)mem) < 0)
+        syserr("shmdt");
+    
+    if(shmctl(shmid, IPC_RMID, 0))
+        syserr("shmctl");
+
+    if(semctl(semid, 0, IPC_RMID, 0))
+        syserr("semctl");
 
     exit(EXIT_SUCCESS);
 }
